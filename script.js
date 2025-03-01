d297d48eb55b4f03488be5ef06fed74f4f9e6bcb
Ayush Dwivedi <dwivediayush028@gmail.com>
2025-03-01 18:59:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+function findDOMLevel() {
+	let element = document.querySelector("#level");
+
+	if(!element){
+		alert("No element with level id found");
+		return;
+	}
+	let level =1;
+	while(element.parentElement){
+		element = element.parentElement;
+		level++
+	}
+	alert(`The level of the element is: ${level}`)
+}
